73c20d1f13c045fd12c21acafd88b626bede050b
omatestisivu <moikamoisule@gmail.com>
2026-05-14 17:15:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const audio = document.getElementById('audiobook');
+const toggleBtn = document.getElementById('toggleBtn');
+const btnText = document.getElementById('btnText');
+const statusText = document.getElementById('statusText');
 
+toggleBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        btnText.innerText = "Pysäytä";
+        statusText.innerText = "Toistetaan äänikirjaa...";
+    } else {
+        audio.pause();
+        btnText.innerText = "Jatka kuuntelua";
+        statusText.innerText = "Keskeytetty";
+    }
+});
+
+// Kun äänikirja loppuu luonnostaan
+audio.onended = () => {
+    btnText.innerText = "Aloita alusta";
+    statusText.innerText = "Äänikirja päättyi";
+};
